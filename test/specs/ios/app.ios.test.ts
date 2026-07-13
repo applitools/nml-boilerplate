39e51f1d1c6b9fda216b909c0bee2af40d4b87f5
Shreya Sharma <shreyasocial@gmail.com>
2026-07-13 12:45:46 +0530

    Add boilerplate for local/typescript-wdio-appium-ios-multi-device

--- test/specs/ios/app.ios.test.ts
+++ test/specs/ios/app.ios.test.ts
@@ -8,10 +8,11 @@ describe('App NML - Local', () => {
     const config = new Configuration();
     config.setUseDom(true);
     config.setSendDom(true);
+    config.addMultiDeviceTarget("iPhone 11 Pro", "iPhone 13");
     eyes.setConfiguration(config);
 
     eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
-    eyes.setBatch(new BatchInfo('TS Local | NML | iOS Boilerplate'));
+    eyes.setBatch(new BatchInfo('TS Local | NML | iOS Boilerplate | Multi Device'));
 
     // TODO: swap in your own app name/description here.
     await eyes.open(browser, 'Local iOS App', 'iOS App Validation');
