933aa18f4dc85a989777ef309ed3ecb2777de9b2
Shreya Sharma <shreya.sharma@applitools.com>
2026-07-17 15:34:33 +0530

    Use ClassicRunner and add eyes_local.log handler; rename spec to _nml_multidevice

--- wdio.ios.local.conf.js
+++ wdio.ios.local.conf.js
@@ -15,7 +15,7 @@ delete caps['appium:optionalIntentArguments'];
 
 export const config = {
 
-  specs: ['./test/specs/ios/app.ios.test.js'],
+  specs: ['./test/specs/ios/app_nml_multidevice.ios.test.js'],
 
   maxInstances: 1,
 
